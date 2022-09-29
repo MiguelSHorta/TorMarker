22f48e7371bcb043aed7b90eed26db75eb0c0b2e
Miguel H <miguelsachorta@gmail.com>
2022-09-29 17:04:22 +0100

    Main update, both components

---
+++ Watermarker/src/InteractiveClient.java
@@ -0,0 +1,197 @@
+import Utils.PcapManager;
+import Utils.Stats;
+import com.opencsv.exceptions.CsvValidationException;
+import org.pcap4j.core.NotOpenException;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class InteractiveClient {
+    public static int remote_port_secure;
+    public static int remote_port_unsecure;
+    public static String remote_host; //
+    public static final int BUF_SIZE = 2048;
+    private static byte[] payloadBytes = null;
+    private static byte[] ack;
+    private static long waitMilli = 0;
+    //private static long waitNano = 0;
+
+
+    public static void main(String[] argv) throws Exception {
+        //TIRMMRT certificate for server side authentication
+        System.setProperty("javax.net.ssl.trustStore", "./keystore/tirmmrts");
+        System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+        System.setProperty("javax.net.ssl.trustStorePassword", "password");
+
+        readConfigurationFiles();
+
+        while (true) {
+            readConfigurationFiles();
+            System.out.println("1 path.pcap path.csv numberOfPackets watermarkType amplitude");
+            Scanner inFromUser = new Scanner(System.in);
+            String[] paths = null;
+            int protocol = 0;
+            try {
+                protocol = inFromUser.nextInt();
+                paths = inFromUser.nextLine().trim().split(" ");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.err.println("Usage: protocol: 1-Pcap, 2-TCP, 3-TLS");
+                System.exit(1);
+            }
+            switch (protocol) {
+                case 1:
+                    Socket socket = new Socket(remote_host, remote_port_unsecure);
+                    OutputStream out = socket.getOutputStream();
+                    InputStream in = socket.getInputStream();
+                    ack = new byte[BUF_SIZE];
+                    PcapManager pcapManager = new PcapManager(paths[0], paths[1], paths[2], paths[3], paths[4]);
+                    Thread.sleep(1000);
+                    new Thread(() -> {
+                        while(true) {
+                            if (canSend())
+                                // Thread responsible for sending the packets and waiting the interleaving delays between them
+                                try {
+                                    out.write(payloadBytes);
+                                    //System.out.println(String.format("Writing packet with length: %d and waiting %d milliseconds", payloadBytes.length, waitMilli));
+                                    updatePayload(null);
+                                    if(waitMilli==-1) break;
+                                    Thread.sleep(waitMilli, 0);
+                                } catch (InterruptedException | IOException e) {
+                                    e.printStackTrace();
+                                }
+                        }
+                        System.out.println("Thread closing.");
+                    }).start();
+                    // Always running, canSend()==true means packet has been sent and thread is waiting the interleaving delay; waitMilli==-1 file has reached the end or enough packets have been sent
+                    while(true){
+                        if(!canSend()){
+                            if(waitMilli==-1) break;
+                            advanceValues(pcapManager);
+                        }
+                    }
+                    System.out.println("Finished replaying pcap.");
+
+                    break;
+                case 2: // TIR debugging, ignore
+                    try {
+                        Socket tcp_socket = new Socket(remote_host, remote_port_unsecure);
+                        do_TCP_TLS(tcp_socket, paths[0]);
+                        tcp_socket.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 3: // TIR debugging, ignore
+                    Socket tls_socket = getSecureSocket(remote_host, remote_port_secure);
+                    do_TCP_TLS(tls_socket, paths[0]);
+                    tls_socket.close();
+                    break;
+                default:
+                    System.out.println("No such protocol");
+            }
+        }
+    }
+
+    /**
+     * Gets new interleaving delay and payload from next packet to be sent and updates payloadBytes and waitMilli accordingly
+     * @param pcapManager
+     * @throws NotOpenException
+     * @throws CsvValidationException
+     * @throws IOException
+     */
+    private static synchronized void advanceValues(PcapManager pcapManager) throws NotOpenException, CsvValidationException, IOException {
+        long waitTimingMilli = pcapManager.nextTiming();
+        payloadBytes = pcapManager.nextPacket();
+        waitMilli = waitTimingMilli;
+        //waitNano = timing[1];
+    }
+
+    /**
+     * Checks if payloadBytes is null
+     * @return
+     */
+    private static synchronized boolean canSend() {
+        return payloadBytes != null;
+    }
+
+    /**
+     * Updates the payload to null after sending a packet so thread can start processing another one
+     * @param value
+     */
+    private static synchronized void updatePayload(byte[] value) {
+        payloadBytes = value;
+    }
+
+    /**
+     * Reads the paths from config.properties file
+     */
+    private static void readConfigurationFiles() {
+
+        try {
+            InputStream input = new FileInputStream("./configuration/config.properties");
+            Properties prop = new Properties();
+
+            prop.load(input);
+
+            remote_host = prop.getProperty("remote_host");
+            remote_port_unsecure = Integer.parseInt(prop.getProperty("remote_port_unsecure"));
+            remote_port_secure = Integer.parseInt(prop.getProperty("remote_port_secure"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Opens a socket for TLS secured communication, not used in the dissertation's context
+     * @param host
+     * @param port
+     * @return
+     * @throws IOException
+     */
+    private static Socket getSecureSocket(String host, int port) throws IOException {
+        SSLSocketFactory factory =
+                (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket socket =
+                (SSLSocket) factory.createSocket(host, port);
+        socket.startHandshake();
+        return socket;
+    }
+
+    /**
+     * Simple HTTP requester, for debugging the Tor security enforcement, not used.
+     * @param socket
+     * @param path
+     * @throws IOException
+     */
+    private static void do_TCP_TLS(Socket socket, String path) throws IOException {
+        OutputStream out = socket.getOutputStream();
+        InputStream in = socket.getInputStream();
+        Stats stats = new Stats();
+
+        String message_str = String.format("GET %s HTTP/1.1\r\n\r\n", path);
+        byte[] message = message_str.getBytes();
+        System.out.println(String.format("%b", message));
+        out.write(message, 0, message.length);
+        System.out.println("Sent request: " + message_str + " for " + remote_host + ":" + remote_port_secure);
+        out.flush();
+
+        int n;
+        byte[] buffer = new byte[BUF_SIZE];
+        while ((n = in.read(buffer, 0, buffer.length)) != -1) {
+            stats.newRequest(n);
+            //System.out.write(buffer, 0, n);
+        }
+        stats.printReport();
+    }
+
+}
+
---
+++ Watermarker/src/Utils/PcapManager.java
@@ -0,0 +1,277 @@
+package Utils;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import org.pcap4j.core.*;
+import org.pcap4j.packet.Packet;
+
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+
+public class PcapManager {
+    private PcapHandle capHandler;
+    private CSVReader timingsReader;
+    InputStream in;
+    double previousTiming;
+    int packetsToSend;
+    int counter;
+    int flowSize = 350;
+    boolean divideFlow = false;
+    double[][] intervals = {{12,8.5,8,7.5}, {10.5,4.5,5.5,9}, {0.5,13.5,1.5,3}, {11,2.5,7,5}};
+    double intervalSize = 0.5;
+    long maxAbsDelay = 80;
+    long timingBuffer = 50;
+    String watermarkType;
+    long[] delays;
+    long amplitude;
+    Random generator = new Random(1l);
+    String capturePath;
+    String timingsPath;
+
+    public PcapManager(String capturePath, String timingsPath, String packetsToSend, String watermarkType, String amplitude) throws PcapNativeException, NotOpenException, IOException, CsvValidationException, InterruptedException {
+        this.capturePath = capturePath;
+        this.timingsPath = timingsPath;
+        openCapture();
+        this.watermarkType = watermarkType;
+        this.packetsToSend = Integer.parseInt(packetsToSend);
+        previousTiming = 0;
+        this.amplitude = Long.parseLong(amplitude);
+        embedWatermark();
+    }
+
+    /**
+     * Opens the capture file with the packets and .csv with the interleaving timings
+     * @throws PcapNativeException
+     * @throws IOException
+     * @throws NotOpenException
+     * @throws CsvValidationException
+     * @throws InterruptedException
+     */
+    private void openCapture() throws PcapNativeException, IOException, NotOpenException, CsvValidationException, InterruptedException {
+        System.out.println("Attempting to open capture file in path: " + capturePath);
+        capHandler = Pcaps.openOffline(capturePath);
+
+        System.out.println("Attempting to open csv file in path: " + timingsPath);
+        timingsReader = new CSVReader(new FileReader(timingsPath));
+        timingsReader.readNext();
+        previousTiming = Double.parseDouble(timingsReader.readNext()[0]);
+        counter = 0;
+    }
+
+    /**
+     * Crafts the interleaving times for the specific watermarks or just processes the real times in case no watermark is selected.
+     * @throws CsvValidationException
+     * @throws IOException
+     * @throws NotOpenException
+     * @throws PcapNativeException
+     * @throws InterruptedException
+     */
+    private void embedWatermark() throws CsvValidationException, IOException, NotOpenException, PcapNativeException, InterruptedException {
+
+        String line[];
+        delays = new long[packetsToSend];
+        double totalReplayTime = 0;
+        switch (watermarkType.toLowerCase()){
+            case "rainbow":
+                System.out.println("Crafting Rainbow watermark...");
+                long minDelta = 0;
+                long maxDelta = 0;
+                while(maxAbsDelay != Math.abs(minDelta) || maxAbsDelay != Math.abs(maxDelta)) {
+                    long currentDelta = 0;
+                    minDelta = 0;
+                    maxDelta = 0;
+                    for (int i = 0; i < packetsToSend; i++) {
+
+                        if(Math.abs(currentDelta) == maxAbsDelay)
+                            if(currentDelta < maxAbsDelay) currentDelta += amplitude;
+                            else currentDelta -= amplitude;
+                        else if (generator.nextDouble() < 0.4999) currentDelta += amplitude;
+                        else currentDelta -= amplitude;
+
+                        if (currentDelta < minDelta) minDelta = currentDelta;
+                        if (currentDelta > maxDelta) maxDelta = currentDelta;
+
+                            delays[i] = currentDelta + maxAbsDelay;
+                        System.out.print(String.format("|%d", delays[i]));
+                    }
+
+                    //System.out.print(minDelta + " ");
+                    //System.out.println(maxDelta);
+                }
+
+                //for (int i = 1; i < 24000; i++) {
+                //    timingsReader.readNext();
+                //}
+
+                //line = timingsReader.readNext();
+                //Double currentTiming = (Double.parseDouble(line[0]));
+                //previousTiming = currentTiming;
+
+                System.out.println();
+                for (int i = 0; i < packetsToSend; i++) {
+                    if ((line = timingsReader.readNext()) != null) {
+                        Double currentTiming = (Double.parseDouble(line[0]));
+                        long microIPD = Math.round((currentTiming - previousTiming) * 1000000);
+                        previousTiming = currentTiming;
+                        delays[i] += (microIPD / 1000);
+                        totalReplayTime += delays[i];
+                        System.out.print(String.format("|%d", delays[i]));
+                    } else delays[i] = -1;
+                }
+
+                System.out.println(String.format("\nFinished crafting watermark, base delay set to %d", maxAbsDelay));
+
+                break;
+
+            case "icbw":
+                System.out.println("Crafting icbw watermark...");
+                double currentInterval = -1;
+                Double currentTiming;
+                /*for (int i = 1; i < 0; i++) timingsReader.readNext();
+                    line = timingsReader.readNext();
+                    currentTiming = (Double.parseDouble(line[0]));
+                    previousTiming = currentTiming;*/
+
+                for (int i = 0; i < packetsToSend; i++) {
+                    if ((line = timingsReader.readNext()) != null) {
+                        currentTiming = (Double.parseDouble(line[0]));
+                        long microIPD = Math.round((currentTiming - previousTiming) * 1000000);
+                        previousTiming = currentTiming;
+
+                        while(currentTiming > 15) currentTiming-=15;
+
+                        boolean found = false;
+                        if(currentTiming > currentInterval && currentTiming < currentInterval+intervalSize){
+                            found = true;
+                            delays[i] = amplitude + (microIPD / 1000);
+                        } else for (int j = 0; j < 4 && !found; j++)
+                            for (int k = 0; k < 4 && !found; k++)
+                                if(currentTiming > intervals[j][k] && currentTiming < intervals[j][k]+intervalSize) {
+                                    currentInterval = intervals[j][k];
+                                    delays[i] = amplitude + (microIPD / 1000);
+                                    found = true;
+                                }
+
+                        if(!found) delays[i] = (microIPD / 1000);
+
+                        System.out.print(String.format("|%d", delays[i]));
+                        totalReplayTime += delays[i];
+                    } else delays[i] = -1;
+                }
+
+                System.out.println(String.format("\nFinished crafting watermark, base delay set to %d", maxAbsDelay));
+
+                break;
+
+            default:
+                System.out.println("Replaying pcap...");
+                for (int i = 0; i < packetsToSend; i++)
+                    if ((line = timingsReader.readNext()) != null) {
+                        currentTiming = (Double.parseDouble(line[0]));
+                        long microIPD = Math.round((currentTiming - previousTiming) * 1000000);
+                        delays[i] = (microIPD / 1000);
+                        previousTiming = currentTiming;
+                        totalReplayTime += delays[i];
+                    } else delays[i] = -1;
+        }
+
+        System.out.printf("Replay time will be %f", (totalReplayTime/1000) + (timingBuffer*packetsToSend/1000));
+    }
+
+    public synchronized long nextTiming() throws NotOpenException, CsvValidationException, IOException {
+
+        long waitTimingMilli = delays[counter++];
+        if(counter==packetsToSend)
+            return -1;
+        //if(divideFlow && counter%flowSize<2) waitTimingMilli+=3500;
+        //System.out.println(waitTimingMilli - maxDelay);
+
+        return waitTimingMilli + timingBuffer;
+    }
+
+    public byte[] nextPacket() throws NotOpenException {
+        return capHandler.getNextPacket().getPayload().getPayload().getPayload().getRawData();
+    }
+
+    // Debugging purposes ignore
+    public void replayPcap() throws CsvValidationException, IOException, NotOpenException, PcapNativeException, InterruptedException {
+        System.out.println("Replaying the capture file...");
+
+        double currentTiming = 0;
+        String[] line = timingsReader.readNext();
+        Packet packet;
+        byte[] payloadBytes = null;
+        long microIPD = 0;
+        long milliIPD = 0;
+        long nanoIPD = 0;
+
+
+        /*while ((packet = capHandler.getNextPacket()) != null && (line = timingsReader.readNext()) != null) {
+            currentTiming = (Double.parseDouble(line[0]));
+            microIPD = Math.round((currentTiming - previousTiming) * 1000000);
+            milliIPD = microIPD / 1000;
+            nanoIPD = (microIPD - milliIPD * 1000) * 1000;
+            payloadBytes = packet.getPayload().getPayload().getPayload().getRawData();
+            previousTiming = currentTiming;
+
+            Thread.sleep(milliIPD, (int) nanoIPD);
+
+            //System.out.println(String.format("Packet length: %d Raw data length: %d Payload: ->%s", packet.length(), packet.getRawData().length,  packet.getPayload().getPayload().getPayload().length()));
+            out.write(payloadBytes);
+            //System.out.println(String.format("CSVTiming: %.6f CSVLength: %s PcapLength: %d", timing, line[1], packet.length()));
+        }*/
+        //long processing;
+        /*while (true) {
+            //processing = System.nanoTime();
+            packet = capHandler.getNextPacket();
+            line = timingsReader.readNext();
+            if (packet == null || line == null) break;
+            currentTiming = (Double.parseDouble(line[0]));
+            microIPD = Math.round((currentTiming - previousTiming) * 1000000);
+            milliIPD = microIPD / 1000;
+            nanoIPD = (microIPD - milliIPD * 1000) * 1000;
+            payloadBytes = packet.getPayload().getPayload().getPayload().getRawData();
+            previousTiming = currentTiming;
+
+            Thread.sleep(milliIPD, (int) nanoIPD);
+
+            //System.out.println(String.format("Packet length: %d Raw data length: %d Payload: ->%s", packet.length(), packet.getRawData().length,  packet.getPayload().getPayload().getPayload().length()));
+            out.write(payloadBytes);
+            //System.out.println(String.format("CSVTiming: %.6f CSVLength: %s PcapLength: %d", timing, line[1], packet.length()));
+        }*/
+
+        while (true) {
+            packet = capHandler.getNextPacket();
+            line = timingsReader.readNext();
+            if (packet == null || line == null) break;
+            currentTiming = (Double.parseDouble(line[0]));
+            milliIPD = Math.round((currentTiming - previousTiming) * 1000);
+            payloadBytes = packet.getPayload().getPayload().getPayload().getRawData();
+            previousTiming = currentTiming;
+
+            Thread.sleep(milliIPD);
+
+            //out.write(payloadBytes);
+            //System.out.println(String.format("Packet length: %d Raw data length: %d Payload: ->%s", packet.length(), packet.getRawData().length,  packet.getPayload().getPayload().getPayload().length()));
+            //System.out.println(String.format("CSVTiming: %.6f CSVLength: %s PcapLength: %d", timing, line[1], packet.length()));
+        }
+
+        System.out.println("Finished replaying the file.");
+    }
+
+    // Debugging purposes ignore
+    public void replaySinglePacket(int n) throws NotOpenException, PcapNativeException {
+        Packet packet = null;
+        for (int i = 0; i < n; i++) {
+            packet = capHandler.getNextPacket();
+        }
+        System.out.println("Sending packet:\n" + packet);
+        //injector.sendPacket(packet);
+    }
+
+}
+
